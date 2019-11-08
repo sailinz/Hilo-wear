c4059caf28fa8124fbe64b61d9d114e5f11f2dd1
Sailin ZHONG <sailin.zhong@unifr.ch>
2019-11-08 17:15:06 +0100

    Initial Commit

---
+++ Hilo-iwatch-1 WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Hilo-iwatch-1 WatchKit Extension
+//
+//  Created by ZHONG Sailin on 08.11.19.
+//  Copyright © 2019 ZHONG Sailin. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Hilo-iwatch-1 WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Hilo-iwatch-1 WatchKit Extension
+//
+//  Created by ZHONG Sailin on 08.11.19.
+//  Copyright © 2019 ZHONG Sailin. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
